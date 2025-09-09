84653b3d6194d42bcc2398e9c16c623380383e47
eremiah3 <111817409+eremiah3@users.noreply.github.com>
2025-09-09 17:04:38 +0100

    commit

--- public/script.js
+++ public/script.js
@@ -78,6 +78,7 @@ document.addEventListener('DOMContentLoaded', () => {
       if (url.includes("youtu.be/")) videoId = url.split("youtu.be/")[1].split(/[?&]/)[0];
       else if (url.includes("watch?v=")) videoId = url.split("watch?v=")[1].split("&")[0];
       else if (url.includes("/live/")) videoId = url.split("/live/")[1].split(/[?&]/)[0];
+       else if (url.includes("shorts")) videoId = url.split("shorts")[1].split("&")[0];
       else if (url.includes("/embed/")) return url;
       if (videoId) return `https://www.youtube.com/embed/${videoId}`;
       return url;
